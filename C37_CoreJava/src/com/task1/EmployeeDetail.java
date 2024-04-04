826aa27ff3873db35650466529ec8142c7b5108d
VS RAJU <VS RAJU@172.16.143.118>
2024-04-04 15:16:37 +0530

    Rakshi

---
+++ C37_CoreJava/src/com/task1/EmployeeDetail.java
@@ -0,0 +1,12 @@
+package com.task1;
+
+public class EmployeeDetail {
+	public void employeeId() {
+		System.out.println("Rakshita Id is 3118");
+	}
+	private void employeeDetail() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
