724cf1209462e5d1082a0c28740efb2227ed9a20
VS RAJU <VS RAJU@DESKTOP-7LQT8EN>
2024-04-10 00:17:54 +0530

    rachuu

---
+++ C37_CoreJava/src/com/tns/daytwo/IfElseDemo.java
@@ -0,0 +1,12 @@
+package com.tns.daytwo;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		int age=11;
+	if(age >= 18)
+		System.out.println("You Are Eligible for Voting");
+	else
+		System.out.println("You have to wait for more");
+		System.out.println("if else demonstration");
+}
+}
