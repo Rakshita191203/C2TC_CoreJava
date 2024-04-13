c385a12ae21d6747e103523e2465b7b5653f9b47
VS RAJU <VS RAJU@DESKTOP-7LQT8EN>
2024-04-13 13:05:57 +0530

    Day Three

---
+++ C37_CoreJava/src/com/tns/daythree/PersonDemo.java
@@ -0,0 +1,31 @@
+package com.tns.daythree;
+
+import java.util.Scanner;
+
+public class PersonDemo {
+	public static void main(String[] args) {
+		
+		Scanner ob = new Scanner(System.in);
+		String name;
+		System.out.println("Enter Person Name : ");
+		name = ob.next();
+		System.out.println("Enter age: ");
+		int age = ob.nextInt();
+		System.out.println("Enter gender: ");
+		String gender = ob.next();
+		System.out.println("Enter taxable income: ");
+		int income = ob.nextInt();
+		
+		Person person = new Person();
+		person.setName(name);
+		person.setAge(age);
+		person.setGender(gender);
+		person.setIncome(income);
+		System.out.println(person);
+		ob.close();
+		}
+
+	}
+
+
+
