8922e7592b39f19eeedeb2c8286fb2977ac1e1b2
VS RAJU <VS RAJU@DESKTOP-7LQT8EN>
2024-04-14 23:41:52 +0530

    Day Five

---
+++ C37_CoreJava/src/com/tns/dayfive/HierarchicalIneritenceDemo.java
@@ -0,0 +1,19 @@
+package com.tns.dayfive;
+
+public class HierarchicalIneritenceDemo {
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		System.out.println("----------------- Person Details ---------------------");
+		System.out.println(p1);
+		Person p;
+		p = new Person("Dhruv", "Mumbai");
+		if (p instanceof Person)
+		System.out.println("Person Details "+p);
+		p = new Employee("Nikhil", "Mumbai", 101, 67000, "Sales");
+		if (p instanceof Employee)
+		System.out.println("Employee Details "+p);
+		p = new Studentt("Pankaj", "Pune", "FE", 88);
+		if (p instanceof Studentt)
+		System.out.println("Student Details "+p);
+	}
+}
---
+++ C37_CoreJava/src/com/tns/dayfive/Studentt.java
@@ -0,0 +1,37 @@
+package com.tns.dayfive;
+
+public class Studentt  extends Person{
+	private String clas;
+	private float per;
+	public Studentt() {
+	System.out.println("Student class default constructor");
+	clas="FY";
+	per=70;
+	}
+	public Studentt(String clas, float per) {
+	this.clas = clas;
+	this.per = per;
+	}
+	public String getClas() {
+	return clas;
+	}
+	public void setClas(String clas) {
+	this.clas = clas;
+	}
+	public float getPer() {
+	return per;
+	}
+	public void setPer(float per) {
+	this.per = per;
+	}
+	public Studentt(String name, String city,String clas, float per )
+	{
+	super(name,city);
+	this.clas = clas;
+	this.per = per;
+	}
+	@Override
+	public String toString() {
+	return "Student [clas=" + clas + ", per=" + per + ", getName()=" + getName() + ", getCity()=" + getCity() + "]";
+	}
+}
