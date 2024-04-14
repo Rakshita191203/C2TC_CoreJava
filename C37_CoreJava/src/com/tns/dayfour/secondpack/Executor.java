23c6860ac8054c0c3a73a8d84c62d03e6d846496
VS RAJU <VS RAJU@DESKTOP-7LQT8EN>
2024-04-14 15:28:41 +0530

    Day Four FirstPack

---
+++ C37_CoreJava/src/com/tns/dayfour/secondpack/Executor.java
@@ -0,0 +1,15 @@
+package com.tns.dayfour.secondpack;
+
+import com.tns.dayfour.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+		Base b1 = new Base();
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+		}
+
+	}
+
+
