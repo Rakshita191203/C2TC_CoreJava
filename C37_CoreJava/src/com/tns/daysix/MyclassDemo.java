fd3cfb6a471990bbfff333e4bbec8db825c1c1f3
VS RAJU <VS RAJU@DESKTOP-7LQT8EN>
2024-04-20 17:25:08 +0530

    Day six

---
+++ C37_CoreJava/src/com/tns/daysix/MyclassDemo.java
@@ -0,0 +1,17 @@
+package com.tns.daysix;
+
+public class MyclassDemo extends MyClass{
+	public static void main(String[] args) {
+		MyClass o1 = new MyClass();
+		System.out.println(o1);
+		
+		MyClass.display();
+		MyClass o2 = new MyClass();
+		System.out.println(o2);
+		MyClass.display();
+		MyClass o3 = new MyClass();
+		System.out.println(o3);
+		MyClass.display();
+	}
+
+}
